77ba290a66545f9d15edeb9066d6b205ed2e44f0
Muhammad Gad <gadm19@gmail.com>
2019-03-02 11:31:17 +0200

    second commit

--- config/database.js
+++ config/database.js
@@ -1,9 +1,13 @@
+const MongoClient = require('mongodb').MongoClient;
+
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: `mongodb://mgad:il0veallah@cluster0-shard-00-00-e2mph.gcp.mongodb.net:27017,cluster0-shard-00-01-e2mph.gcp.mongodb.net:27017,cluster0-shard-00-02-e2mph.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true`
+        mongoURI: `mongodb://mgad:<PASSWORD>@mycluster-shard-00-00-e2mph.mongodb.net:27017,mycluster-shard-00-01-e2mph.mongodb.net:27017,mycluster-shard-00-02-e2mph.mongodb.net:27017/test?ssl=true&replicaSet=myCluster-shard-0&authSource=admin&retryWrites=true`
     }
 } else {
     module.exports = {
         mongoURI: `mongodb://localhost/vidjot-dev`
     }
 }
+
+
