fbb503545bde170728b3d4efff2538327fe8fbd5
Muhammad Gad <gadm19@gmail.com>
2019-03-02 11:50:36 +0200

    third commit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const MongoClient = require('mongodb').MongoClient;
 
 if (process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: `mongodb://mgad:<PASSWORD>@mycluster-shard-00-00-e2mph.mongodb.net:27017,mycluster-shard-00-01-e2mph.mongodb.net:27017,mycluster-shard-00-02-e2mph.mongodb.net:27017/test?ssl=true&replicaSet=myCluster-shard-0&authSource=admin&retryWrites=true`
+        mongoURI: `mongodb://mgad:il0veallah@mycluster-shard-00-00-e2mph.mongodb.net:27017,mycluster-shard-00-01-e2mph.mongodb.net:27017,mycluster-shard-00-02-e2mph.mongodb.net:27017/test?ssl=true&replicaSet=myCluster-shard-0&authSource=admin&retryWrites=true`,
+        secretOrkey: 'secret'
     }
 } else {
     module.exports = {
